5ec380b74344bcb5fb97192724b59998b6acb0ee
Elena Pedko <elpg.ods@gmail.com>
2024-03-12 23:05:39 +0200

    routes are created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+
+const Home = lazy(() => import('./pages/Home'));
+const Movies = lazy(() => import('./pages/Movies'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails'));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element = {<Navigate to='popular'/>}/>
+        <Route path='popular' element={<Home/>}/>
+        <Route path='search' element={<Movies/>}/>
+        <Route path='/:movieId' element={<MovieDetails/>}/>
+      </Route>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Loader from '../../components/Loader/Loader';
+
+const Layout = () => {
+  return (
+    <>
+      <Header/>
+      <Suspense fallback={<Loader/>}>
+        <Outlet/>
+      </Suspense>
+      <Footer/>
+    </>
+  )
+}
+
+export default Layout;
