062c4a02c7088bb4694da7f5e986795fa3c67777
Elena Pedko <elpg.ods@gmail.com>
2024-07-11 15:25:38 +0300

    misprint is corrected

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="movies">
+    <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
